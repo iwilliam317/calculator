0795372ba900ac9a6f7daa3e2aa683ce8078a71e
william <willshinji@gmail.com>
2019-06-05 19:42:01 -0300

    change digit from int to string

--- src/calculator/index.jsx
+++ src/calculator/index.jsx
@@ -10,29 +10,29 @@ class Calculator extends Component {
             <>
                <h1>Calculator</h1>
                <div className='calculator'>
-                   <Display value={100} />
+                   <Display value='100' />
                    
                    <Button style='operator' digit='AC' />
                    <Button style='operator' digit='/' />
                    <Button style='operator' digit='-' />
                    <Button style='operator' digit='/' />
 
-                   <Button digit={7} />
-                   <Button digit={8} />
-                   <Button digit={9} />
+                   <Button digit='7' />
+                   <Button digit='8' />
+                   <Button digit='9' />
                    <Button style='operator' digit='x' />
 
-                   <Button digit={4} />
-                   <Button digit={5} />
-                   <Button digit={6} />
+                   <Button digit='4' />
+                   <Button digit='5' />
+                   <Button digit='6' />
                    <Button style='operator' digit='-' />
 
-                   <Button digit={1} />
-                   <Button digit={2} />
-                   <Button digit={3} />
+                   <Button digit='1' />
+                   <Button digit='2' />
+                   <Button digit='3' />
                    <Button style='operator' digit='+' />
                    
-                   <Button style='double' digit={0} />
+                   <Button style='double' digit='0' />
                    <Button digit=',' />
                    <Button style='operator' digit='=' />
                </div>
