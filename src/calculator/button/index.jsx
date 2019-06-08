15da5a38e295f82da2c4707da6f7240f7cfebb8d
william <willshinji@gmail.com>
2019-06-07 22:35:57 -0300

    minor refactor

--- src/calculator/button/index.jsx
+++ src/calculator/button/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './style.css'
 
-const Button = ({digit, style, addDigit}) => (
-    <button type='button' className={`button ${style || ''}`} onClick={addDigit}>{digit}</button>
+const Button = ({digit, style, click}) => (
+    <button type='button' className={`button ${style || ''}`} onClick={click}>{digit}</button>
 )
 
 export default Button
--- src/calculator/index.jsx
+++ src/calculator/index.jsx
@@ -5,38 +5,47 @@ import Display from './display'
 import './style.css'
 
 class Calculator extends Component {
-    state = { currentValue: 0}
+
+    state = { currentValue: 0 }
+
     addDigit = event => {
         const currentValue = this.state.currentValue + event.target.innerHTML
         this.setState({ currentValue })
     }
 
+    clearDisplay = () => {
+        console.log('asd');
+        
+        this.setState({...this.state, currentValue: 0})
+    }
+
     render(){
         const {currentValue} = this.state
+        const {clearDisplay, addDigit} = this
         return(
             <>
                <h1>Calculator</h1>
                <div className='calculator'>
                    <Display value={currentValue} />
                    
-                   <Button style='operator' digit='AC' />
+                   <Button style='operator' digit='AC' click={clearDisplay}/>
                    <Button style='operator' digit='/' />
                    <Button style='operator' digit='-' />
                    <Button style='operator' digit='/' />
 
-                   <Button digit='7' addDigit={this.addDigit}/>
-                   <Button digit='8' addDigit={this.addDigit} />
-                   <Button digit='9' addDigit={this.addDigit} />
-                   <Button style='operator' digit='x' addDigit={this.addDigit} />
+                   <Button digit='7' click={addDigit}/>
+                   <Button digit='8' click={addDigit} />
+                   <Button digit='9' click={addDigit} />
+                   <Button style='operator' digit='x' />
 
-                   <Button digit='4' addDigit={this.addDigit} />
-                   <Button digit='5' addDigit={this.addDigit} />
-                   <Button digit='6' addDigit={this.addDigit} />
+                   <Button digit='4' click={addDigit} />
+                   <Button digit='5' click={addDigit} />
+                   <Button digit='6' click={addDigit} />
                    <Button style='operator' digit='-' />
 
-                   <Button digit='1' addDigit={this.addDigit} />
-                   <Button digit='2' addDigit={this.addDigit} />
-                   <Button digit='3' addDigit={this.addDigit} />
+                   <Button digit='1' click={addDigit} />
+                   <Button digit='2' click={addDigit} />
+                   <Button digit='3' click={addDigit} />
                    <Button style='operator' digit='+' />
                    
                    <Button style='double' digit='0' />
